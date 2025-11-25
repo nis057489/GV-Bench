3d2013b612d00b6c5c4a707386c8ecd411c461bb
Nic <nicholas.j.smith-2@student.uts.edu.au>
2025-11-25 11:01:56 +1100

    force logout script
    Signed-off-by: Nic <nicholas.j.smith-2@student.uts.edu.au>

--- nuclear_logout.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# confirm logout of user nijsmith with Y/N
-read -p "Are you sure you want to logout user nijsmith? (Y/N): " choice
-if [[ "$choice" == "yes_logout" ]]; then
-    pkill -KILL -u nijsmith
-    echo "User nijsmith has been logged out."
-else
-    echo "Logout cancelled."
-fi
